9656ec60a7db262d187a2effc5845da57e6e8418
Korewa-blip <murekosh@gmail.com>
2025-07-19 12:39:33 +0100

    Add files via upload

---
+++ Datatypecourse.py
@@ -0,0 +1,12 @@
+number = 10
+number2 = 10.5
+name= "Korewa"
+result = False
+print(type(number))
+print(type(number2))
+print(type(name))
+print(type(result))
+number3=int(number2)
+print(number3)
+number4=float(number)
+print(number4)
